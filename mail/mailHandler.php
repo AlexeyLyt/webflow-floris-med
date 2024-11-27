5e6fdbac1644368d6f27c7e4e415136d3f4b6d8e
Aleksey Lytovchenko <litovchenko>
2024-11-28 01:30:11 +0300

    upd mailHandler.php

--- mail/mailHandler.php
+++ mail/mailHandler.php
@@ -1,25 +1,58 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-	echo "<script>console.log('123');</script>";
+	$formName = $_POST['form_name'] ?? '';
+	echo "<script>console.log('$formName');</script>";
+
+	// reCAPTCHA
 	$secret = '6Leim_UpAAAAAH1rKA5DLLOyqFwTUmqA-5CwCzC2';
-	$name = $_POST['Name'];
-	$tel = $_POST['Phone'];
-	$email = $_POST['Email'];
-	$honeypot = $_POST['honeypot'];
 	$recaptchaResponse = $_POST['g-recaptcha-response'];
+	$honeypot = $_POST['honeypot'];
+
+	$name = $_POST['Name'];
+	$phone = $_POST['Phone'];
+	$email = $_POST['Email'];
+	$subject = $_POST['Subject']; // Услуга
+	$formHeaderName = null;
+	$message = null;
+	$isValid = true;
 
 	// Запрос к API reCAPTCHA для проверки токена
 	$verifyResponse = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret={$secret}&response={$recaptchaResponse}");
 	$responseData = json_decode($verifyResponse);
 
-	$headers = 'From: ' . 'alelytovc@yandex.ru' . "\r\n" .
-		'Reply-To: ' . 'alelytovc@yandex.ru' . "\r\n" .
-		'X-Mailer: PHP/' . phpversion();
+	// Настройки почты
+	$to = "katowicelyt@yandex.ru";
+	$headers = "From: alelytovc@yandex.ru\r\n";
+	$headers .= "Content-Type: text/plain; charset=utf-8";
+
+	switch ($formName) {
+		case 'wf-form-Contact-Form':
+			$formHeaderName = 'Заявка с главной формы сайта';
+			$name = htmlspecialchars($_POST['Name'] ?? '');
+			$phone = htmlspecialchars($_POST['Phone'] ?? '');
+			$email = htmlspecialchars($_POST['Email'] ?? '');
+			$formMessage = htmlspecialchars($_POST['Message'] ?? '');
+			if (!$name && !$phone && !$email && !$formMessage) $isValid = false;
+			$message = "Имя: $name \nПочта: $email \nТелефон: $phone \nУслуга: $subject \nСообщение: $formMessage";
+			break;
+
+		case 'feedback_form':
+			$username = htmlspecialchars($_POST['username'] ?? '');
+			$feedback = htmlspecialchars($_POST['feedback'] ?? '');
+
+			$subject = "Отзыв пользователя";
+			$message = "Имя пользователя: $username\nОтзыв: $feedback";
+			break;
+
+		default:
+			echo "Неизвестная форма!";
+			exit;
+	}
 
 	// Проверка ответа от reCAPTCHA
 	// if ($responseData->success && $responseData->score >= 0.5) {
-	if ((!$name && !$tel && !$email) || $honeypot) return;
-	if (mail("katowicelyt@yandex.ru", "Заявка с главной страницы сайта", "Имя: " . $name . "\r\n" . "Телефон: " . $tel . "\r\n" . "Почта: " . "$email", $headers)) {
+	if (!$isValid || $honeypot) return;
+	if (mail($to, $formHeaderName, $message, $headers)) {
 		// echo "Сообщение успешно отправлено";
 		header('Location: /contact-form-handler.html?status=success');
 	} else {
