8e04eafd71809a7c3d88c98fdb07b6cc532b1472
Aleksey Lytovchenko <litovchenko>
2024-11-27 23:25:28 +0300

    test logic for mail through php mail() & xampp + placeholder reCaptcha (not working locally)

---
+++ mail/mailHandler.php
@@ -0,0 +1,32 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	echo "<script>console.log('123');</script>";
+	$secret = '6Leim_UpAAAAAH1rKA5DLLOyqFwTUmqA-5CwCzC2';
+	$name = $_POST['Name'];
+	$tel = $_POST['Phone'];
+	$email = $_POST['Email'];
+	$honeypot = $_POST['honeypot'];
+	$recaptchaResponse = $_POST['g-recaptcha-response'];
+
+	// Запрос к API reCAPTCHA для проверки токена
+	$verifyResponse = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret={$secret}&response={$recaptchaResponse}");
+	$responseData = json_decode($verifyResponse);
+
+	$headers = 'From: ' . 'alelytovc@yandex.ru' . "\r\n" .
+		'Reply-To: ' . 'alelytovc@yandex.ru' . "\r\n" .
+		'X-Mailer: PHP/' . phpversion();
+
+	// Проверка ответа от reCAPTCHA
+	// if ($responseData->success && $responseData->score >= 0.5) {
+	if ((!$name && !$tel && !$email) || $honeypot) return;
+	if (mail("katowicelyt@yandex.ru", "Заявка с главной страницы сайта", "Имя: " . $name . "\r\n" . "Телефон: " . $tel . "\r\n" . "Почта: " . "$email", $headers)) {
+		// echo "Сообщение успешно отправлено";
+		header('Location: /coming-soon.html?status=success');
+	} else {
+		echo "При отправке сообщения возникли ошибки";
+	}
+	// } else {
+	//     // Если проверка не прошла - выводим ошибку
+	//     echo 'Verification failed. Please try again';
+	// }
+}
